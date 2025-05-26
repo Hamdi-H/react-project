fea2985b541a50f8c064161d51a8f1b8b1ffba73
Hamdi-H <hamdi.hussen134@gmail.com>
2025-05-26 12:07:07 +0300

    added usefetch file to the component and used some hooks too

--- src/components/Create.js
+++ src/components/Create.js
@@ -48,17 +48,17 @@ return (
         <Form onSubmit={handleSubmit}>
             <Form.Group className='mb-3' controlId='exampleForm.controlInput1'>
                 <Form.Label>Title</Form.Label>
-                <Form.Control name='title' type='text' onChange={handleChange} placeholder='Enter the Title'/>
+                <Form.Control name='title' type='text' onChange={handleChange} placeholder='Enter the Title' required/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
                     <Form.Label>Author</Form.Label>
-                    <Form.Control name="author" type="text" onChange={handleChange} placeholder="Enter author's name" />
+                    <Form.Control name="author" type="text" onChange={handleChange} placeholder="Enter author's name"required/>
                 </Form.Group>
               
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                     <Form.Label>Body</Form.Label>
-                    <Form.Control name="body" as="textarea" rows={3} onChange={handleChange} placeholder="Enter body" />
+                    <Form.Control name="body" as="textarea" rows={3} onChange={handleChange} placeholder="Enter body" required/>
                 </Form.Group>
                
                 <Button variant='primary' type='submit'>Submit</Button>
